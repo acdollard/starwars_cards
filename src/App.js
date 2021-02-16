6c0f3f9ecf8b7366ccc19113ad1479ac92f0b132
alexDollard <alexdollard141@gmail.com>
2021-02-16 14:54:50 -0600

    basic API search call running w/ info displayed onscreen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {styles} from './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    const [people, setPeople] = useState('Darth Vader');
+    const [results, setResults] = useState([])
+
+    useEffect(() => {
+       const search = async () => {
+            const { data } = await axios.get(`https://swapi.dev/api/people/?search=${people}`);
+            setResults(data.results[0]);
+        };
+        search();
+    }, [people])
+
+    console.log(results);
+
+    return (
+        <div className="main-window">
+            <div className="form">
+                <label>Search for characters</label>
+                <input 
+                        className="input"
+                        value={people}
+                        onChange={(e) => setPeople(e.target.value)}
+                        />
+                
+            </div>
+            <div className="character-data">
+                <h1>{results.name}</h1>
+                <p>gender: {results.gender}</p>
+                <p>eye color: {results.eye_color}</p>
+            </div>
+        
+        </div>
+    )
 }
 
 export default App;
